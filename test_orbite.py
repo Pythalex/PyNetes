39a2bf16cfc2757818a95735232236c5e1e8e341
Alexandre <alexandre.alex10@hotmail.fr>
2017-03-05 23:21:04 +0100

    Ajout d'options dans l'interface
    Interface non terminée mais fonctionnelle

--- test_orbite.py
+++ test_orbite.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from Tkinter import *
 
 WIDTH     = 640
@@ -7,6 +8,7 @@ t = 0                             # variable de temps
 timecount = 0
 UA        = 100                    # UA <-> px
 TIMESPEED = 3000000               # Accélération du temps
+NBPLANETE = 0
 
 days      = 0
 daysPyimage = -1
@@ -36,15 +38,10 @@ class Planete(object):
 	def actualiser_position(self, force = False):
 		# Si la vérification des types est activée et les équations sont valides
 		# Ou si le forcemode est activé
-		if(not(force)                                                  and \
-		   type(self.equationx) == sage.symbolic.expression.Expression and \
-		   type(self.equationy) == sage.symbolic.expression.Expression or  \
-		   force):
-
+		if(not(force) and type(self.equationx) == sage.symbolic.expression.Expression and type(self.equationy) == sage.symbolic.expression.Expression or force):
 			self.posx = self.equationx(t).n() + ORIGIN[0]
 			self.posy = self.equationy(t).n() + ORIGIN[1]
 			
-
 	def get_pos(self):
 		return (self.posx, self.posy)
 
@@ -69,10 +66,7 @@ def animation(master, canvas, planete):
 	# equation horaire
 	planete.actualiser_position(force = True)
 	# Affichage planète
-	planete.pyimage = canvas.create_circle(x = planete.get_pos()[0], 
-									       y = planete.get_pos()[1], 
-						 				   r = planete.taille,
-						 				   fill = "#5555FF")
+	planete.pyimage = canvas.create_circle(x = planete.get_pos()[0], y = planete.get_pos()[1], r = planete.taille, fill = "#5555FF")
 
 	# Actualisation canvas
 	canvas.update_idletasks()
@@ -103,38 +97,85 @@ def daysText(canvas):
 	global daysPyimage
 
 	canvas.delete(daysPyimage)
-	daysPyimage = canvas.create_text(DAYSTEXTAREA[0],
-									 DAYSTEXTAREA[1],
-								     text = "Jours : {}".format(days))
-
+	daysPyimage = canvas.create_text(DAYSTEXTAREA[0], DAYSTEXTAREA[1], text = "Jours : {}".format(days))
 
 def main():
+	def SelectPlanete(terre, satelite):
+		if terre.get():
+			Terre         = Planete(taille = 5, distance_Etoile = 1, vitesseAng = 1.99e-7)
+			equationTerre = (Terre.distanceUA * cos(Terre.vitesseAng * x), Terre.distanceUA * sin(Terre.vitesseAng * x))
+			Terre.set_equation(equationTerre[0], equationTerre[1])
+			animation(fen, can, Terre)
+
+		if satelite.get():
+			Satellite   = Planete(taille = 3, distance_Etoile = 0.5, vitesseAng = 1.99e-7)
+			equationSat = (Satellite.distanceUA * cos(Satellite.vitesseAng * x + pi / 5), Satellite.distanceUA * sin(Satellite.vitesseAng * x + pi / 5))
+			Satellite.set_equation(equationSat[0], equationSat[1])
+			animation(fen, can, Satellite)
+
+		if not terre.get():
+			pass
+
+		if not satelite.get():
+			pass
+	
+	def AutrePlanete(taille, distance_Etoile, vitesseAng):
+		global NBPLANETE
+		vari            = globals()
+		taille          = float(taille.get())
+		distance_Etoile = float(distance_Etoile.get())
+		vitesseAng      = float(vitesseAng.get())
+		
+		vari["Planetenb" + str(NBPLANETE)]         = Planete(taille, distance_Etoile, vitesseAng)
+		vari["EquationPlanetenb" + str(NBPLANETE)] = (vari["Planetenb" + str(NBPLANETE)].distanceUA * cos(vari["Planetenb" + str(NBPLANETE)].vitesseAng * x + pi / 5), vari["Planetenb" + str(NBPLANETE)].distanceUA * sin(vari["Planetenb" + str(NBPLANETE)].vitesseAng * x + pi / 5))
+		vari["Planetenb" + str(NBPLANETE)].set_equation(vari["EquationPlanetenb" + str(NBPLANETE)][0], vari["EquationPlanetenb" + str(NBPLANETE)][1])
+		animation(fen, can, vari["Planetenb" + str(NBPLANETE)])
+		NBPLANETE += 1
 
 	fen = Tk()
 
 	can = Canvas(fen, width = WIDTH, height = HEIGHT)
-	can.pack()
+	can.grid(row=1, column=1, rowspan=2)
+
+	terre_select    = IntVar()
+	satelite_select = IntVar()
+	custom_select1  = StringVar()
+	custom_select2  = StringVar()
+	custom_select3  = StringVar()
+
+	select_planets  = LabelFrame(fen, text="Planètes")
+
+	select_planets1 = Checkbutton(select_planets, text="Terre", variable = terre_select)
+	select_planets2 = Checkbutton(select_planets, text="Satelite", variable = satelite_select)
+	bouton_actu     = Button(select_planets, text="Actualiser", command=lambda : SelectPlanete(terre_select, satelite_select))
+
+	create_planets        = LabelFrame(fen, text="Création")
+	label_create_planets1 = Label(create_planets, text="Taille")
+	create_planets1       = Entry(create_planets, textvariable=custom_select1)
+	label_create_planets2 = Label(create_planets, text="Distance")
+	create_planets2       = Entry(create_planets, textvariable=custom_select2)
+	label_create_planets3 = Label(create_planets, text="Vitesse")
+	create_planets3       = Entry(create_planets, textvariable=custom_select3)
+	bouton_create         = Button(create_planets, text="Créer", command=lambda : AutrePlanete(custom_select1, custom_select2, custom_select3))
+
+	select_planets.grid(row=1, column=2)
+	select_planets1.pack()
+	select_planets2.pack()
+
+	create_planets.grid(row=2, column=2)
+	label_create_planets1.pack()
+	create_planets1.pack()
+	label_create_planets2.pack()
+	create_planets2.pack()
+	label_create_planets3.pack()
+	create_planets3.pack()
+	
+	bouton_create.pack()
+	bouton_actu.pack()
 
 	can.create_circle(ORIGIN[0], ORIGIN[1], 10, fill = "#FFFF00")
 
-	Terre = Planete(taille = 5, 
-					distance_Etoile = 1, 
-					vitesseAng = 1.99e-7)
-	equationTerre = (Terre.distanceUA * cos(Terre.vitesseAng * x), 
-					 Terre.distanceUA * sin(Terre.vitesseAng * x))
-	Terre.set_equation(equationTerre[0], equationTerre[1])
-
-	Satellite = Planete(taille = 3,
-						distance_Etoile = 0.5,
-						vitesseAng = 1.99e-7)
-	equationSat = (Satellite.distanceUA * cos(Satellite.vitesseAng * x + pi / 5),
-				   Satellite.distanceUA * sin(Satellite.vitesseAng * x + pi / 5))
-
-	Satellite.set_equation(equationSat[0], equationSat[1])
-
 	time_actualise(fen, can)
-	animation(fen, can, Terre)
-	animation(fen, can, Satellite)
 	
 	fen.mainloop()
 
